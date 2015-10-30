45b36f9c4d620a0a0fb1256430317616e9dec251
LeaGaona <lea.gaona@telecom-st-etienne.fr>
2015-10-30 12:53:21 +0100

    modification second commit

--- TestGitHub/src/tse/info/test_1.java
+++ TestGitHub/src/tse/info/test_1.java
@@ -4,6 +4,7 @@ public class test_1 {
 	public static void main(String[] args) {
 		System.out.println("Test de code sur Git");
 		System.out.println("modification ");
+		System.out.println("modification 2");
 	}
 
 }
